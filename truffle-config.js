89b044d50512183fdce63d2e4d793df4a85cbdaa
thinhle-itdeveloper-work <thinhle.itdeveloper.work@gmail.com>
2020-01-27 22:47:54 +0700

    Modify the truffle-config.js file

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+var mnemonic = "elegant pottery logic donkey baby chat tackle keen maximum stem siren virtual"
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -42,11 +44,21 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    /*
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/3f03375705584ad0a4b50d877ba8d490", 0)
+      },
+      network_id: 3,          // Ropsten's id
+      host: "localhost",
+      port:  1502,
+    },
+    */
 
     // Another network with more advanced options...
     // advanced: {
